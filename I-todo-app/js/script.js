954fd3230fa9d076427ae17952628526cf3eb3dc
1Hanif1 <hanifmohammed869@gmail.com>
2022-05-24 23:12:12 +0530

    Added Delete and Complete functionality

--- I-todo-app/js/script.js
+++ I-todo-app/js/script.js
@@ -14,11 +14,62 @@ let localStorageTasks, taskCount;
 const userInput = document.querySelector('.userInput');
 const taskContainer = document.querySelector('.main__tasks');
 
+const clearAllTasks = function () {
+    taskContainer.querySelectorAll()
+}
+const deleteTask = function (todoDiv) {
+    const todoTask = todoDiv.querySelector('.task').textContent;
+    todoDiv.remove()
+    for (let key of Object.keys(localStorageTasks)) {
+        if (localStorageTasks[key]?.todo == todoTask) {
+            delete localStorageTasks[key]
+            break;
+        };
+    }
+    localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
+    containerVisible();
+    countTasks();
+}
+const markTask = function (todoDiv) {
+    const todoTask = todoDiv.querySelector('.task').textContent;
+    if (todoDiv.classList.contains('done')) {
+        todoDiv.classList.remove('done');
+        todoDiv.querySelector('.circle').classList.remove('done');
+        todoDiv.querySelector('.task').classList.remove('done');
+
+        for (let key of Object.keys(localStorageTasks)) {
+            if (localStorageTasks[key]?.todo == todoTask) {
+                localStorageTasks[key].completed = false;
+                break;
+            };
+        }
+
+        localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
+    }
+    else {
+        todoDiv.classList.add('done');
+        todoDiv.querySelector('.circle').classList.add('done');
+        todoDiv.querySelector('.task').classList.add('done');
+
+        for (let key of Object.keys(localStorageTasks)) {
+            if (localStorageTasks[key]?.todo == todoTask) {
+                localStorageTasks[key].completed = true;
+                break;
+            };
+        }
+
+        localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
+    }
+}
+taskContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('cross')) deleteTask(e.target.closest('.task__container'));
+    if (e.target.classList.contains('circle')) markTask(e.target.closest('.task__container'));
+})
+
 const countTasks = function () {
     let totalTask = document.querySelectorAll('.task__container ').length;
     document.querySelector('.totalTask').textContent = totalTask;
 }
-
 const saveTask = function (task, id) {
     let taskObject = {
         'todo': task,
@@ -27,11 +78,11 @@ const saveTask = function (task, id) {
     localStorageTasks[id] = taskObject;
     localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
 }
-const renderTask = function (task, id) {
+const renderTask = function (task, id, status) {
     let html = `
-    <div class="task__container " data-id="${id}">
-        <div class="circle"></div>
-        <div class="task">${task}</div>
+    <div class="task__container ${status ? "done" : ""}" data-id="${id}">
+        <div class="circle ${status ? "done" : ""}"></div>
+        <div class="task ${status ? "done" : ""}">${task}</div>
         <img class="cross" src="./images/icon-cross.svg" alt="/">
     </div>
     `
@@ -42,11 +93,10 @@ userInput.addEventListener('keydown', function (e) {
     let task = userInput.value;
     ++taskCount;
     saveTask(task, taskCount);
-    renderTask(task, taskCount);
+    renderTask(task, taskCount, false);
     containerVisible();
     countTasks();
 })
-
 
 const loadTasks = function () {
     localStorageTasks = localStorage.getItem('tasks');
@@ -58,7 +108,8 @@ const loadTasks = function () {
         localStorageTasks = JSON.parse(localStorageTasks);
         for (const id in localStorageTasks) {
             let task = localStorageTasks[id]["todo"];
-            renderTask(task, id);
+            let status = localStorageTasks[id]["completed"]
+            renderTask(task, id, status);
             taskCount = id;
         }
         countTasks();
