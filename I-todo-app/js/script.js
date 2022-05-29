e75a537db354e85a1508586d0aec198f6011dfe6
1Hanif1 <hanifmohammed869@gmail.com>
2022-05-29 13:00:19 +0530

    javascript files

--- I-todo-app/js/script.js
+++ I-todo-app/js/script.js
@@ -1,141 +1,39 @@
 'use strict';
 
-const deleteTask = function (todoDiv) {
-    const todoTask = todoDiv.querySelector('.task').textContent;
-    todoDiv.remove()
-    for (let key of Object.keys(localStorageTasks)) {
-        if (localStorageTasks[key]?.todo == todoTask) {
-            delete localStorageTasks[key]
-            break;
-        };
-    }
-    localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
-    containerVisible();
-    countTasks();
-}
-const markTask = function (todoDiv) {
-    const todoTask = todoDiv.querySelector('.task').textContent;
-    if (todoDiv.classList.contains('done')) {
-        todoDiv.classList.remove('done');
-        todoDiv.querySelector('.circle').classList.remove('done');
-        todoDiv.querySelector('.task').classList.remove('done');
+themeSwitcher.addEventListener('click', function () {
+    if (!sunIcon.classList.contains('hide')) {
+        // change sun/moon icon
+        sunIcon.classList.add('hide');
+        moonIcon.classList.remove('hide');
 
-        for (let key of Object.keys(localStorageTasks)) {
-            if (localStorageTasks[key]?.todo == todoTask) {
-                localStorageTasks[key].completed = false;
-                break;
-            };
-        }
+        // change background
+        bgDesktop.forEach(img => {
+            if (img.classList.contains('bg-d-l')) img.classList.add('hide');
+            else img.classList.remove('hide');
+        })
+        bgMobile.forEach(img => {
+            if (img.classList.contains('bg-m-l')) img.classList.add('hide');
+            else img.classList.remove('hide');
+        })
 
-        localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
-    }
-    else {
-        todoDiv.classList.add('done');
-        todoDiv.querySelector('.circle').classList.add('done');
-        todoDiv.querySelector('.task').classList.add('done');
-
-        for (let key of Object.keys(localStorageTasks)) {
-            if (localStorageTasks[key]?.todo == todoTask) {
-                localStorageTasks[key].completed = true;
-                break;
-            };
-        }
-
-        localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
-    }
-}
-
-const clearCompleted = function () {
-    const completedTasks = document.querySelectorAll('.task__container.done');
-    completedTasks.forEach(taskContainer => {
-
-        let id = +taskContainer.dataset.id;
-        taskContainer.remove();
-        delete localStorageTasks[id];
-        localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
-    })
-    containerVisible();
-    countTasks();
-}
-
-const hideTasks = function () {
-    const allTasks = document.querySelectorAll('.task__container');
-    allTasks.forEach(task => task.remove());
-}
-
-const showAll = function () {
-    hideTasks();
-    loadTasks();
-
-}
-
-const resetSortBtns = function () {
-    document.querySelectorAll('.sort').forEach(btn => btn.style.color = '');
-}
-
-const showActive = function () {
-    showAll();
-    const completedTasks = document.querySelectorAll('.task__container.done')
-    completedTasks.forEach(task => task.style.display = 'none')
-
-}
-
-const showCompleted = function () {
-    showAll();
-    const Tasks = document.querySelectorAll('.task__container')
-    Tasks.forEach(task => {
-        if (!task.classList.contains('done')) task.style.display = 'none';
-    })
-
-}
-
-const countTasks = function () {
-    let totalTask = document.querySelectorAll('.task__container ').length;
-    document.querySelector('.totalTask').textContent = totalTask;
-}
-const saveTask = function (task, id) {
-    let taskObject = {
-        'todo': task,
-        'completed': false,
-    }
-    localStorageTasks[id] = taskObject;
-    localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
-}
-const renderTask = function (task, id, status) {
-    let html = `
-    <div class="task__container ${status ? "done" : ""}" data-id="${id}">
-        <div class="circle ${status ? "done" : ""}"></div>
-        <div class="task ${status ? "done" : ""}">${task}</div>
-        <img class="cross" src="./images/icon-cross.svg" alt="/">
-    </div>
-    `
-    taskContainer.insertAdjacentHTML('afterbegin', html);
-}
-
-const loadTasks = function () {
-    localStorageTasks = localStorage.getItem('tasks');
-    if (!localStorageTasks) {
-        localStorageTasks = {};
-        localStorage.setItem('tasks', JSON.stringify(localStorageTasks))
-        taskCount = 0;
+        // color
+        isDarkMode()
     } else {
-        localStorageTasks = JSON.parse(localStorageTasks);
-        for (const id in localStorageTasks) {
-            let task = localStorageTasks[id]["todo"];
-            let status = localStorageTasks[id]["completed"]
-            renderTask(task, id, status);
-            taskCount = id ? id : 0;
-        }
-        countTasks();
-    }
-}
+        sunIcon.classList.remove('hide');
+        moonIcon.classList.add('hide');
+        bgDesktop.forEach(img => {
+            if (img.classList.contains('bg-d-l')) img.classList.remove('hide');
+            else img.classList.add('hide');
+        })
+        bgMobile.forEach(img => {
+            if (img.classList.contains('bg-m-l')) img.classList.remove('hide');
+            else img.classList.add('hide');
+        })
 
-const containerVisible = function () {
-    if (Object.keys(localStorageTasks).length > 0)
-        taskContainer.style.visibility = 'visible';
-    else
-        taskContainer.style.visibility = 'hidden';
-}
+        // Color
+        isDarkMode()
+    }
+})
 
 taskContainer.addEventListener('click', function (e) {
     if (e.target.classList.contains('cross')) deleteTask(e.target.closest('.task__container'));
@@ -156,7 +54,6 @@ userInput.addEventListener('keydown', function (e) {
     containerVisible();
     countTasks();
 })
-
 
 window.addEventListener('load', function () {
     taskCount = 0;
--- I-todo-app/js/variables.js
+++ I-todo-app/js/variables.js
@@ -1,4 +1,11 @@
 let localStorageTasks, taskCount;
 const userInput = document.querySelector('.userInput');
 const taskContainer = document.querySelector('.main__tasks');
-
+const themeSwitcher = document.querySelector('.toggle');
+const sunIcon = document.querySelector('.sun');
+const moonIcon = document.querySelector('.moon');
+const bgDesktop = document.querySelectorAll('.bg-desktop');
+const bgMobile = document.querySelectorAll('.bg-mobile');
+const inputBox = document.querySelector('.main__input')
+const circles = document.querySelectorAll('.circle');
+const sortingBtns = document.querySelector('.controls')
