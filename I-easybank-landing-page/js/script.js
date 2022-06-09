17dfe90eb8c19f27d28e5e3e48d067c1d716bc62
1Hanif1 <hanifmohammed869@gmail.com>
2022-06-09 23:59:30 +0530

    javascript

--- I-easybank-landing-page/js/script.js
+++ I-easybank-landing-page/js/script.js
@@ -4,6 +4,49 @@ const hamburgerMenu = document.querySelector("#menuBtn");
 const overlay = document.querySelector('.overlay');
 const headerMenu = document.querySelector('.header__menu');
 const body = document.querySelector('body');
+const featureContainer = document.querySelector('.feature');
+const articleContainer = document.querySelector('.articles');
+
+const observerOptions = { root: null, threshold: 0.4 }
+
+const features = document.querySelectorAll('.feature__item');
+const featureObserver = new IntersectionObserver(
+    (e) => {
+        if (!e[0].isIntersecting) return;
+        let miliseconds = 200;
+        features.forEach(
+            feature => {
+                setTimeout(
+                    () => feature.classList.add('show-card'),
+                    miliseconds
+                );
+                miliseconds += 100;
+            }
+        )
+
+    },
+    observerOptions
+);
+featureObserver.observe(featureContainer);
+
+const articles = document.querySelectorAll('.article__item');
+const articleObserver = new IntersectionObserver(
+    (e) => {
+        if (!e[0].isIntersecting) return;
+        let miliseconds = 200;
+        articles.forEach(
+            article => {
+                setTimeout(
+                    () => article.classList.add('show-card'),
+                    miliseconds
+                );
+                miliseconds += 100;
+            }
+        )
+    },
+    observerOptions
+)
+articleObserver.observe(articleContainer);
 
 function hamburgerToggleFunction() {
     header.classList.toggle('open')
